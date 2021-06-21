7bd19a31b01fed05aeea0c0473a6a0a627321b2c
Kamil Wiśniewski <kamil.wisniewski@student.wat.edu.pl>
2021-06-21 03:23:26 +0200

    hotfix v4

--- src/main/java/Federates/GUI/GUIFederate.java
+++ src/main/java/Federates/GUI/GUIFederate.java
@@ -15,6 +15,15 @@ public class GUIFederate extends BaseFederate {
 
     public ArrayList<Car> cars;
 
+    public ArrayList<Integer> StartedCarsSize = new ArrayList<>();
+    public Integer OverallQueue1Size = 0;
+    public Integer OverallQueue2Size = 0;
+    public ArrayList<Integer> Queue1Size = new ArrayList<>();
+    public ArrayList<Integer> Queue2Size = new ArrayList<>();
+    public ArrayList<Integer> LightsTimer = new ArrayList<>();
+    public ArrayList<Integer> BridgeSide = new ArrayList<>();
+    public ArrayList<Integer> GeneratedCars = new ArrayList<>();
+
     public void initializeFederate() {
         this.federateType = "GUIFederateType";
     }
@@ -45,7 +54,15 @@ public class GUIFederate extends BaseFederate {
         cars = Car.makeCarModel(sendCarData.get("carIds"), sendCarData.get("carSpeeds"), sendCarData.get("carCurrentStates"), sendCarData.get("carSides"));
     }
 
-    public void receiveStats(HashMap<String, String> sendStats) {
+    public void receiveStats(HashMap<String, String> parameters) {
+        StartedCarsSize = makeArray(parameters.get("StartedCarsSize").split(","));
+        OverallQueue1Size = Integer.parseInt(parameters.get("OverallQueue1Size"));
+        OverallQueue2Size = Integer.parseInt(parameters.get("OverallQueue2Size"));
+        Queue1Size = makeArray(parameters.get("Queue1Size").split(","));
+        Queue2Size = makeArray(parameters.get("Queue2Size").split(","));
+        LightsTimer = makeArray(parameters.get("LightsTimer").split(","));
+        BridgeSide = makeArray(parameters.get("BridgeSide").split(","));
+        GeneratedCars = makeArray(parameters.get("GeneratedCars").split(","));
     }
 
     public static void main(String[] args) {
@@ -60,5 +77,14 @@ public class GUIFederate extends BaseFederate {
         }
     }
 
+    public ArrayList<Integer> makeArray(String[] tmpStringArray)
+    {
+        ArrayList<Integer> tmpArray = new ArrayList<>();
+        for(String string:tmpStringArray)
+        {
+            tmpArray.add(Integer.parseInt(string));
+        }
+        return tmpArray;
+    }
 
 }
